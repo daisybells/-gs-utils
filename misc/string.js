263dcc376c0c431d5bb701a456d3ad6c915179a7
gwen <cubinggamer@gmail.com>
2025-09-28 16:06:42 -0400

    added clear-white-space to string.js

--- misc/string.js
+++ misc/string.js
@@ -12,5 +12,16 @@ function normalizeString(string, inputOptions) {
 function capitalizeWords(string) {
     return string.replaceAll(/\b\w(?!\s)/giu, (letter) => letter.toUpperCase());
 }
+function clearWhiteSpace(htmlString = "") {
+    const specialWhiteSpaceCharacters = /[\t\n\v\f\r]+/gu;
+    const stringWithoutSpecialSpaces = htmlString.replaceAll(
+        specialWhiteSpaceCharacters,
+        ""
+    );
+    const collapsedString = stringWithoutSpecialSpaces
+        .replaceAll(/  +/gu, " ")
+        .trim();
+    return collapsedString;
+}
 
-export { normalizeString, capitalizeWords };
+export { normalizeString, capitalizeWords, clearWhiteSpace };
